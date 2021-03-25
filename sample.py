f45820b7128b6237608487490bd16baf72d1e275
wkn816 <wkn816@gmail.com>
2021-03-25 18:09:40 +0900

    add-値と値の方の出力

--- sample.py
+++ sample.py
@@ -1,1 +1,10 @@
-print('Hello')
+# print('Hello')
+number = 1
+number2 = 1.0
+greeting = "こんにちわ"
+is_ok = True
+
+print(number, type(number))
+print(number2, type(number2))
+print(greeting, type(greeting))
+print(is_ok, type(is_ok))
